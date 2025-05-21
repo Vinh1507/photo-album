8b677129f86d827fd6ef71ad1615a5858df312ea
Hoa Ngo <xuanhoa2772004@gmail.com>
2025-05-21 12:14:34 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import React from 'react';
 import '@ant-design/v5-patch-for-react-19';
-import MediaPhotoAlbum from './pages/MediaPhotoAlbum';
+import HikingPhotoAlbum from './pages/HikingPhotoAlbum';
+import './styles/HikingPhotoAlbum.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Ứng dụng Photo Album</h1>
-      <MediaPhotoAlbum />
+      <HikingPhotoAlbum />
     </div>
   );
 }
 
-export default App
+export default App;
---
+++ src/pages/HikingPhotoAlbum.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { MasonryPhotoAlbum } from 'react-photo-album';
+import Lightbox from 'yet-another-react-lightbox';
+import 'yet-another-react-lightbox/styles.css';
+import Zoom from 'yet-another-react-lightbox/plugins/zoom';
+import Thumbnails from 'yet-another-react-lightbox/plugins/thumbnails';
+import Captions from 'yet-another-react-lightbox/plugins/captions';
+import 'yet-another-react-lightbox/plugins/thumbnails.css';
+import 'yet-another-react-lightbox/plugins/captions.css';
+import { usePhotos } from '../data/photos';
+
+const HikingPhotoAlbum = () => {
+  const [index, setIndex] = useState(-1);
+  const { photos, loading } = usePhotos();
+
+  if (loading) {
+    return (
+      <div className="loading">
+        <div className="spinner"></div>
+        <div>Đang tải album ảnh...</div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="photo-album-container">
+      <h1>Album Ảnh</h1>
+      
+      <div className="album-wrapper">
+        <MasonryPhotoAlbum
+          photos={photos}
+          columns={3}
+          spacing={4}
+          padding={5}
+          onClick={({ index }) => setIndex(index)}
+          renderPhoto={({ photo, wrapperProps, imageProps }) => (
+            <div {...wrapperProps} className="photo-wrapper">
+              <img {...imageProps} className="album-image" />
+              <div className="image-overlay">
+                <div className="image-title">{photo.alt}</div>
+              </div>
+            </div>
+          )}
+        />
+      </div>
+      
+      <Lightbox
+        slides={photos}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        plugins={[Zoom, Thumbnails, Captions]}
+        captions={{ descriptionTextAlign: 'center' }}
+        zoom={{ maxZoomPixelRatio: 3 }}
+        thumbnails={{ width: 100, height: 80 }}
+      />
+    </div>
+  );
+};
+
+export default HikingPhotoAlbum; 
